a25f41279309f68318140fa53076a2c16b3290ee
misterkalik <misterkalik@gmail.com>
2025-09-04 23:23:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,22 +4,68 @@
 const tutylowa = document.getElementById("tytulowa");
 const oferta = document.getElementById("tytulowaOferta");
 const guzik = document.getElementById("ofertaGuzik");
-const logoNapis = document.getElementById("logoNapis");
+const ofertaPowrot = document.getElementById("ofertaPowrot");
 
 
+//Formularz
+const inputZdjecia = document.getElementById('zdjecia');
+const plikLista = document.querySelector('.plikLista');
+
+// Tablica przechowująca wybrane pliki
+let selectedFiles = [];
 
 //FUNKCJE--------------------------
 
 
 //strona glowna
+if(guzik){
 guzik.addEventListener("click", function(){
     tytulowa.classList.remove("aktywna");
     oferta.classList.add("aktywna");
     
-});
+})};
 
-logoNapis.addEventListener("click", function(){
+if(ofertaPowrot){
+ofertaPowrot.addEventListener("click", function(){
     oferta.classList.remove("aktywna");
     tytulowa.classList.add("aktywna");
-});
+})};
 
+
+//Formularz
+if(inputZdjecia){
+    inputZdjecia.addEventListener('change', function() {
+        if (this.files.length > 0) {
+          // dodajemy nowe pliki do tablicy (unikalne)
+          Array.from(this.files).forEach(file => {
+            if (!selectedFiles.some(f => f.name === file.name)) {
+              selectedFiles.push(file);
+            }
+          });
+        }
+      
+        renderFileList();
+      });
+};
+
+function renderFileList() {
+    plikLista.innerHTML = '';
+  
+    if (selectedFiles.length === 0) {
+        plikLista.innerHTML = '<li>Brak plików</li>';
+      return;
+    }
+  
+    selectedFiles.forEach((file, index) => {
+      const li = document.createElement('li');
+      li.textContent = file.name;
+  
+      // kliknięcie usuwa plik
+      li.addEventListener('click', () => {
+        selectedFiles.splice(index, 1);
+        renderFileList();
+      });
+  
+      plikLista.appendChild(li);
+    });
+  }
